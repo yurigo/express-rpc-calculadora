4ad6fb8fe20b506fccd6aaa3ac32ff8ca1523d21
Yuri <ygonzoo@gmail.com>
2021-04-23 15:21:00 +0200

    heroku

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
 
-const port="3000"
+var port = process.env.PORT || 8080;
 
-const c = require("./controller")
+const c = require("./controller");
 
-app.get('/', c.hello );
-app.get('/suma', c.suma );
-app.get('/resta', c.resta );
-app.get('/multiplicacion', c.multiplicacion );
-app.get('/division', c.division );
+app.get("/", c.hello);
+app.get("/suma", c.suma);
+app.get("/resta", c.resta);
+app.get("/multiplicacion", c.multiplicacion);
+app.get("/division", c.division);
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
+    console.log(`http://localhost:${port}`);
 });
